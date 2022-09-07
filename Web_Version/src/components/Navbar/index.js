c863ef42d80dc28ffb9167e6f02747047ef4762c
jdminer2 <jdminer2@ncsu.edu>
2022-09-07 12:10:22 -0400

    Implemented navbar dropdown icon.
    When the window is resized too small to see the page options, there is an icon for a dropdown menu to select a page.
    It was not implemented before but now it is implemented and can take the user to different pages.

--- Web_Version/src/components/Navbar/index.js
+++ Web_Version/src/components/Navbar/index.js
@@ -8,12 +8,29 @@ import {
     NavBtn,
     NavBtnLink,
 } from './NavbarElements';
+import Menu from '@mui/material';
 
+
+/** This is the top green bar that allows the user to navigate all the pages */
 const Navbar = () => {
+    // Variables for opening and closing dropdown menu
+    const [menuAnchor, setMenuAnchor] = React.useState(null);
+    const open = Boolean(menuAnchor);
+
+    // Functions for opening and closing dropdown menu
+    function handleBarsClick(event) {
+        setMenuAnchor(event.currentTarget);
+    }
+    function handleCloseMenu(event) {
+        setMenuAnchor(null);
+    }
+
     return (
         <>
             <Nav>
-                <Bars />
+                {/* Button to view a dropdown menu if screen is too small to list all options */}
+                <Bars onClick={handleBarsClick}/>
+                {/* List of options*/}
                 <NavMenu>
                     <NavLink to='/distributed_load' activeStyle>
                         Distributed Load
@@ -35,6 +52,32 @@ const Navbar = () => {
                     </NavLink>
                 </NavMenu>
             </Nav>
+            {/* Dropdown menu listing all buttons */}
+            <Menu
+                anchorEl={menuAnchor}
+                open={open}
+                onClose={handleCloseMenu}
+                onClick={handleCloseMenu}
+            >
+                <NavLink to='/distributed_load' activeStyle>
+                    Distributed Load
+                </NavLink>
+                <NavLink to='/single_load' activeStyle>
+                    Single Load
+                </NavLink>
+                <NavLink to='/multiple_loads' activeStyle>
+                    Multiple Load
+                </NavLink>
+                <NavLink to='/ce325'>
+                    CE 325
+                </NavLink>
+                <NavLink to='/cantilever_beam'>
+                    Cantilever Beam
+                </NavLink>
+                <NavLink to='/combined_load'>
+                    Combined Loads
+                </NavLink>
+            </Menu>
         </>
     );
 };
